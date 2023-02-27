373c20f1ab458df6f8b35df9d9b4adc166fb8373
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-02-27 23:08:34 +0100

    replication: drop is_joining flag
    Its single usecase was to allow instance_id change during join.
    This was allowed assuming that at rebootstrap an instance will
    fetch not the latest master's state but its latest snapshot file.
    And then the changes from xlogs.
    
    Those xlogs could contain a deletion of this replica from _cluster
    if the master tried to clear the space from dead replicas.
    
    But join/reboostrap since 2.3.1 doesn't work this way (6332aca655,
    "relay: join new replicas off read view"). The replica fetches the
    latest master's state right from memory, not from the latest
    snapshot file. Hence it won't see any deletions. Not even just
    from _cluster.
    
    That means this check can become stricter and is_joining flag is
    not needed anymore.
    
    NO_DOC=refactoring
    NO_CHANGELOG=refactoring
    NO_TEST=already covered

--- src/box/alter.cc
+++ src/box/alter.cc
@@ -4031,28 +4031,13 @@ on_replace_dd_cluster(struct trigger *trigger, void *event)
 		on_rollback->data = replica;
 		txn_stmt_on_rollback(stmt, on_rollback);
 	} else {
-		/*
-		 * Don't allow deletion of the record for this instance
-		 * from _cluster.
-		 */
 		assert(old_tuple != NULL);
 		uint32_t replica_id;
 		if (tuple_field_u32(old_tuple, BOX_CLUSTER_FIELD_ID, &replica_id) != 0)
 			return -1;
 		if (replica_check_id(replica_id) != 0)
 			return -1;
-		/*
-		 * It's okay to update the instance id (drop, then insert) while
-		 * it is joining to a cluster as long as the id is set by the
-		 * time bootstrap is complete, which is checked in box_cfg()
-		 * anyway.
-		 *
-		 * For example, the replica could be deleted from the _cluster
-		 * space on the master manually before rebootstrap, in which
-		 * case it will replay this operation during the final join
-		 * stage.
-		 */
-		if (!replicaset.is_joining && replica_id == instance_id) {
+		if (replica_id == instance_id) {
 			diag_set(ClientError, ER_LOCAL_INSTANCE_ID_IS_READ_ONLY,
 				 (unsigned)replica_id);
 			return -1;
